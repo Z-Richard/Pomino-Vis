6892bae22e397a1a86d05c55e94dda52c0ca1f94
Haoyu Zhuang <zhuanghaoyurichard@gmail.com>
2020-08-28 17:11:09 +0800

    deploy setup 3

---
+++ build/precache-manifest.9726c00e6a26b55bbf25ff30cc179a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd69d3b495cb873453716d493e75c73e",
+    "url": "/pomino-vis/index.html"
+  },
+  {
+    "revision": "af406bc73bcc16b6de34",
+    "url": "/pomino-vis/static/css/2.fe7459df.chunk.css"
+  },
+  {
+    "revision": "af406bc73bcc16b6de34",
+    "url": "/pomino-vis/static/js/2.367b9293.chunk.js"
+  },
+  {
+    "revision": "84dca2ba5558a364c768ec3f18f30800",
+    "url": "/pomino-vis/static/js/2.367b9293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74aa4f4be4450ad6e063",
+    "url": "/pomino-vis/static/js/main.2089db49.chunk.js"
+  },
+  {
+    "revision": "f30c4e1cfe3105fb1aaf",
+    "url": "/pomino-vis/static/js/runtime-main.ba75db6c.js"
+  }
+]);
